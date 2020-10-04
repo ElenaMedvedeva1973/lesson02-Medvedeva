f0f7b4974036a98a92d24c2fe18d14aedff6dedb
ElenaMedvedeva1973 <70959696+ElenaMedvedeva1973@users.noreply.github.com>
2020-10-04 19:21:49 +0700

    Add files via upload

---
+++ lesson02 3.py
@@ -0,0 +1,12 @@
+#Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и через dict.
+seasons = {'Winter': (1, 2, 12),
+           'Sping': (3, 4, 5),
+           'Summer': (6, 7, 8),
+           'Autumn': (9, 10, 11)}
+
+month = int(input('Choose a month: '))
+for key in seasons.keys():
+    if month in seasons[key]:
+        print(key)
